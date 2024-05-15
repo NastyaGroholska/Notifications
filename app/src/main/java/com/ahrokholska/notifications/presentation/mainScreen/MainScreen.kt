4c7236aa8efad397b90bdbdf68daba87c6c42784
Anastasiia <anastasiiahrokholska@gmail.com>
2024-05-15 15:04:28 +0300

    Added ability to remove and add pages, scroll between pages

--- app/src/main/java/com/ahrokholska/notifications/presentation/mainScreen/MainScreen.kt
+++ app/src/main/java/com/ahrokholska/notifications/presentation/mainScreen/MainScreen.kt
@@ -1,31 +1,77 @@
 package com.ahrokholska.notifications.presentation.mainScreen
 
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.ahrokholska.notifications.R
 import com.ahrokholska.notifications.presentation.theme.NotificationsTheme
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel) {
-    MainScreenContent()
+    val applicationContext = LocalContext.current.applicationContext
+    MainScreenContent(
+        pages = viewModel.pages.collectAsState().value,
+        onCreateClick = {},
+        onRemoveClick = {
+            viewModel.removePage {
+                Toast.makeText(
+                    applicationContext,
+                    applicationContext.getString(R.string.page_was_removed, it),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        },
+        onAddClick = viewModel::addPage
+    )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun MainScreenContent() {
+fun MainScreenContent(
+    pages: List<Int>,
+    onCreateClick: (Int) -> Unit,
+    onRemoveClick: () -> Unit,
+    onAddClick: (onSuccess: (Int) -> Unit) -> Unit
+) {
+    val coroutineScope = rememberCoroutineScope()
     Scaffold { scaffoldPadding ->
-        Page(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(scaffoldPadding),
-            number = 1,
-            onlyOnePageExists = false,
-            onCreateClick = {},
-            onRemoveClick = {},
-            onAddClick = {}
-        )
+        val pagerState = rememberPagerState(pageCount = { pages.size })
+        HorizontalPager(state = pagerState) { pageIndex ->
+            Page(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(scaffoldPadding),
+                number = pages[pageIndex],
+                onlyOnePageExists = pages.size == 1,
+                onCreateClick = { onCreateClick(pages[pageIndex]) },
+                onRemoveClick = onRemoveClick,
+                onAddClick = {
+                    onAddClick {
+                        coroutineScope.launch {
+                            snapshotFlow { pagerState.pageCount }.collect { pageCount ->
+                                if (pageCount == it) {
+                                    pagerState.scrollToPage(it - 1)
+                                    cancel()
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+        }
     }
 }
 
@@ -33,6 +79,11 @@ fun MainScreenContent() {
 @Composable
 private fun MainScreenPreview() {
     NotificationsTheme {
-        MainScreenContent()
+        MainScreenContent(
+            pages = listOf(1, 2),
+            onCreateClick = {},
+            onRemoveClick = {},
+            onAddClick = {}
+        )
     }
 }
--- app/src/main/java/com/ahrokholska/notifications/presentation/mainScreen/MainScreenViewModel.kt
+++ app/src/main/java/com/ahrokholska/notifications/presentation/mainScreen/MainScreenViewModel.kt
@@ -1,9 +1,38 @@
 package com.ahrokholska.notifications.presentation.mainScreen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor() : ViewModel() {
+    private val _pages = MutableStateFlow(listOf(1))
+    val pages = _pages.asStateFlow()
+
+    fun addPage(onSuccess: (Int) -> Unit) {
+        viewModelScope.launch {
+            var addedPage = 0
+            _pages.update {
+                addedPage = it.last() + 1
+                it + addedPage
+            }
+            onSuccess(addedPage)
+        }
+    }
+
+    fun removePage(onSuccess: (Int) -> Unit) {
+        viewModelScope.launch {
+            var removedPage = 0
+            _pages.update {
+                removedPage = it.last()
+                it.toMutableList().apply { removeLast() }
+            }
+            onSuccess(removedPage)
+        }
+    }
 }
